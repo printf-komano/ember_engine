f7f4ec1c2d84f5c37aac05885f6a42966d003a1f
Decinf <docblaze160@gmail.com>
2025-04-28 01:57:43 +0300

    implementing diffuse soft lighting

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -5,12 +5,17 @@ uniform mat4 view;
 uniform mat4 model;
 
 layout (location = 0) in vec3 pos;
-layout (location = 1) in vec3 clr; 
+layout (location = 1) in vec3 clr;
+layout (location = 2) in vec2 uv;
+layout (location = 3) in vec3 normal;
 
 
+out vec3 frag_normal;
 out vec3 vertex_color;
 
 void main() {
     gl_Position = proj*view*model*vec4(pos, 1.0);
     vertex_color = clr;
+
+    frag_normal = mat3(transpose(inverse(model))) * normal;
 };
