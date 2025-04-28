cd8f8492a4d2a94921b448960e7bb5cb3826608c
Decinf <docblaze160@gmail.com>
2025-04-28 14:58:10 +0300

    flat shading and shader modification; cube bugfix

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -3,12 +3,17 @@
 uniform vec3 light_dir;
 
 
+
+
 in vec3 vertex_color;  //get vertex color
 in vec3 frag_normal;
+in vec3 frag_pos;
 
 out vec4 frag_color;
 
+
 void main(){
-    float light_power = max(dot(frag_normal,light_dir),0.1);
+    vec3 flat_normal = normalize(cross(dFdx(frag_pos),dFdy(frag_pos)));
+    float light_power = clamp(dot(flat_normal,-light_dir),0.1,1.0);
     frag_color = light_power * vec4(vertex_color.r,vertex_color.g,vertex_color.b, 1.0);
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -9,13 +9,16 @@ layout (location = 1) in vec3 clr;
 layout (location = 2) in vec2 uv;
 layout (location = 3) in vec3 normal;
 
-
-out vec3 frag_normal;
-out vec3 vertex_color;
+out vec3 frag_pos;
+out flat vec3 frag_normal;
+out flat vec3 vertex_color;
 
 void main() {
-    gl_Position = proj*view*model*vec4(pos, 1.0);
+    vec4 worldpos = model*vec4(pos,1.0);
+    gl_Position = proj*view*worldpos;
+
     vertex_color = clr;
 
     frag_normal = mat3(transpose(inverse(model))) * normal;
+    frag_pos = worldpos.xyz;
 };
